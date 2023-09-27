501edfc443be7bae36b62ef299ca585238a19ba6
wayne tsao <a578ff6@gmail.com>
2023-09-27 21:12:38 +0800

    Initial Commit

---
+++ Create Programmatic Segues/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Create Programmatic Segues
+//
+//  Created by 曹家瑋 on 2023/9/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
